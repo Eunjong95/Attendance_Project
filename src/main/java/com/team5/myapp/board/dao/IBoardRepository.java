a45c6f8809599d16df12e8dec11e51a996c06405
LeeJiho0304 <leejiho0304@gmail.com>
2023-01-03 17:31:33 +0900

    게시판 인터페이스 및 VO

--- src/main/java/com/team5/myapp/board/dao/IBoardRepository.java
+++ src/main/java/com/team5/myapp/board/dao/IBoardRepository.java
@@ -1,10 +1,39 @@
 package com.team5.myapp.board.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import com.team5.myapp.board.model.Board;
 import com.team5.myapp.board.model.BoardFile;
+import com.team5.myapp.board.model.Comments;
 
 public interface IBoardRepository {
-	void insertArticle(Board board);
+	//게시글 작성, 수정, 삭제
+	void insertBoard(Board board);
 	void insertFileData(BoardFile file);
+	void updateBoard(Board board);
+	void updateFileData(BoardFile file);
+	void deleteFileData(int boardId);
+	void deleteBoard(int boardId);
+	
+	//목록 조회
+	int selectTotalBoardPageByCategoryId(int categoryId);
+	List<Board> selectBoardListByCategory(@Param("categoryId") int categoryId, @Param("start") int start, @Param("end") int end);
+	
+	//게시글 상세보기
+	Board selectBoard(int boardId);
 	BoardFile getFile(int fileId);
+	
+	//조회수 증가
+	void updateReadCount(int boardId);
+
+	//댓글 작성, 수정, 삭제
+	void insertComment(Comments comment);
+	void updateComment(Comments comment);
+	void deleteComment(int commentId);
+	
+	//검색
+	int selectTotalBoardPageByKeyword(String keyword);
+	List<Board> searchListByContentKeyword(@Param("keyword") String keyword, @Param("start") int start, @Param("end") int end);
 }
--- src/main/java/com/team5/myapp/board/service/impl/BoardService.java
+++ src/main/java/com/team5/myapp/board/service/impl/BoardService.java
@@ -1,5 +1,102 @@
 package com.team5.myapp.board.service.impl;
 
-public class BoardService {
+import java.util.List;
+
+import com.team5.myapp.board.model.Board;
+import com.team5.myapp.board.model.BoardFile;
+import com.team5.myapp.board.model.Comments;
+import com.team5.myapp.board.service.IBoardService;
+
+public class BoardService implements IBoardService {
+
+	@Override
+	public void insertBoard(Board board) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insertBoard(Board board, BoardFile file) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateBoard(Board board) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateBoard(Board board, BoardFile file) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteBoard(int boardId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int selectTotalBoardPageByCategoryId(int categoryId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Board> selectBoardListByCategory(int categoryId, int page) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Board selectBoard(int boardId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BoardFile getFile(int fileId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateReadCount(int boardId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insertComment(Comments comment) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateComment(Comments comment) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteComment(int commentId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int selectTotalBoardPageByKeyword(String keyword) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Board> searchListByContentKeyword(String keyword) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
